4a02fb15697e41dbd944bd767c9d2a67989662d2
xiakai <768210094@qq.com>
2019-11-15 14:09:19 +0800

    update 牛客网算法题

--- get-offer/src/NowCoder.java
+++ get-offer/src/NowCoder.java
@@ -118,21 +118,7 @@ public class NowCoder {
         return maxL;
     }
 
-    public static void main(String[] args) throws Exception {
 
-//        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8};
-//        int[] arr2 = {2 ,3, 5, 6, 8 ,9 ,10, 13, 23, 45};
-//        System.out.println(getKthMinFromTwoArr(arr1, arr2, 3));
-//        System.out.println(getKthMinFromTwoArr(arr1, arr2, 5));
-//        System.out.println(getKthMinFromTwoArr(arr1, arr2, 8));
-//        System.out.println(getKthMinFromTwoArr(arr1, arr2, 10));
-//        System.out.println(getKthMinFromTwoArr(arr1, arr2, 15));
-
-        int[] arr = new int[] {1, 2, 1, 1, 1};
-        System.out.println(getLengthOfPlusArrInSum(arr, 3));
-
-
-    }
 
 
 
---
+++ get-offer/src/NowCoderTest.java
@@ -0,0 +1,20 @@
+public class NowCoderTest {
+
+    public static void main(String[] args) throws Exception {
+
+//        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8};
+//        int[] arr2 = {2 ,3, 5, 6, 8 ,9 ,10, 13, 23, 45};
+//        System.out.println(NowCoder.getKthMinFromTwoArr(arr1, arr2, 3));
+//        System.out.println(NowCoder.getKthMinFromTwoArr(arr1, arr2, 5));
+//        System.out.println(NowCoder.getKthMinFromTwoArr(arr1, arr2, 8));
+//        System.out.println(NowCoder.getKthMinFromTwoArr(arr1, arr2, 10));
+//        System.out.println(NowCoder.getKthMinFromTwoArr(arr1, arr2, 15));
+
+
+        int[] arr = new int[] {1, 2, 1, 1, 1};
+        System.out.println(NowCoder.getLengthOfPlusArrInSum(arr, 3));
+
+    }
+
+
+}
