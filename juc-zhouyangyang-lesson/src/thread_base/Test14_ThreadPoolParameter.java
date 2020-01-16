0479b32418583bd6ad1d9bae83a0c7c48bc07bf5
xiakai <768210094@qq.com>
2020-01-16 11:07:45 +0800

    update JUC

--- juc-zhouyangyang-lesson/src/thread_base/Test14_ThreadPoolParameter.java
+++ juc-zhouyangyang-lesson/src/thread_base/Test14_ThreadPoolParameter.java
@@ -1,9 +1,6 @@
 package thread_base;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author xiakai
@@ -14,21 +11,32 @@ public class Test14_ThreadPoolParameter {
     public static void main(String[] args) {
 
 
-        // 核心线程数、最大线程数、超时时间（超时后销毁，直至等于核心线程数）、超时时间单位
-        // 阻塞队列（线程不够用后会进入阻塞队列，队列满后会报拒绝异常）、拒绝异常处理
-        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(5, 10, 10, TimeUnit.SECONDS,
-                new ArrayBlockingQueue<Runnable>(10), new RejectedExecutionHandler() {
-            @Override
-            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-                System.out.println("拒绝");
-            }
-        });
+        // 核心线程数、
+        // 最大线程数、
+        // 超时时间（超时后销毁，直至等于核心线程数）、
+        // 超时时间单位
+        // 阻塞队列（线程不够用后会进入阻塞队列，队列满后会报拒绝异常）、
+        // 默认线程工厂
+        // 拒绝异常处理
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
+                3,
+                5,
+                10,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.CallerRunsPolicy());
 
-        for (int i = 0; i < 22; i++) {
+        // 线程拒绝机制
+        // AbortPolicy 直接抛出默认RejectedExecutionException异常
+        // CallerRunsPolicy 不会抛出异常、也不会抛弃任务，会将任务返回给调用者，从而降低新任务的流量
+        // DiscardOldestPolicy 抛弃队列中等待最久的任务，然后把当前任务加入队列中
+        // DiscardPolicy 不会抛出异常，直接抛弃任务，不做任何处理，这是最好的策略
+        for (int i = 0; i < 20; i++) {
             int finalI = i;
             threadPool.execute(() -> {
                 try {
-                    TimeUnit.SECONDS.sleep(1);
+                    TimeUnit.SECONDS.sleep(3);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
